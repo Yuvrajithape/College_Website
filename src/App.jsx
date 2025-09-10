2d2dafe7c91cbe67aac4eadc1b86513533189a37
Yuvraj Ithape <yuvrajithape9696@gmail.com>
2025-09-10 11:49:29 +0530

    About section is completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './Components/Navbar/Navbar.jsx';
 import Hero from './Components/Hero/hero.jsx'
 import Program from './Components/Program/Program.jsx'
 import Title from './Components/Title/Title.jsx'
+import About from './Components/About/About.jsx'
 
 function App() {
   
@@ -15,8 +16,8 @@ function App() {
       <div className = 'container'>
         <Title subtitle = 'our programs ' title = 'What we Offer'/>
         <Program />
+        <About/>
       </div>
-
      </div>
   )
 }
